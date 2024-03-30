1702959d29de9f0a4d2e2a7cf6b45662eb05000c
Lucas <lucasoliveirars22@gmail.com>
2024-03-30 11:18:57 -0300

    json data

--- App.tsx
+++ App.tsx
@@ -5,16 +5,27 @@ import { theme } from "./src/styles/theme";
 import SecondaryCard from "./src/components/secondary_card";
 
 export default function App() {
+  const secondaryCardsData = [
+    { name: "John Doe", text: "+155", positive: true },
+    { name: "Emily White", text: "+155", positive: true },
+    { name: "Oliver Brown", text: "+155", positive: true },
+    { name: "Ava Miller", text: "-15", positive: false },
+    { name: "Ethan Davis", text: "-15", positive: false },
+    { name: "Sophia Wilson", text: "-15", positive: false },
+  ];
+
   return (
     <View style={styles.container}>
       <PrincipalCard />
 
-      <SecondaryCard name={"John Doe"} text={"+155"} positive={true} />
-      <SecondaryCard name={"Emily White"} text={"+155"} positive={true} />
-      <SecondaryCard name={"Oliver Brown"} text={"+155"} positive={true} />
-      <SecondaryCard name={"Ava Miller"} text={"-15"} positive={false} />
-      <SecondaryCard name={"Ethan Davis"} text={"-15"} positive={false} />
-      <SecondaryCard name={"Sophia Wilson"} text={"-15"} positive={false} />
+      {secondaryCardsData.map((card, index) => (
+        <SecondaryCard
+          key={index}
+          name={card.name}
+          text={card.text}
+          positive={card.positive}
+        />
+      ))}
 
       <StatusBar style="auto" />
     </View>
